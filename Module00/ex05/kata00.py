8db872b6e0e42ea17249d7020e344d12f5e91216
tiza <amaaiza@student.1337.ma>
2023-02-01 22:18:36 +0100

    42

---
+++ Module00/ex05/kata00.py
@@ -0,0 +1,5 @@
+import sys
+kata = (19,42,21)
+if __name__ != "__main__":
+    sys.exit
+print("The 3 numbers are: ", ", ".join(str(x) for x in kata))
---
+++ Module00/ex05/kata01.py
@@ -0,0 +1,8 @@
+kata = {
+'Python': 'Guido van Rossum',
+'Ruby': 'Yukihiro Matsumoto',
+'PHP': 'Rasmus Lerdorf',
+}
+
+for x in kata:
+    print(x,' was created by ', kata[x])
