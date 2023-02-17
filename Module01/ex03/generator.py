8bf506f8087bbbf3486e7f8dceab3ff03cc544d8
hhhh <hhhh@hhhhs-MacBook-Pro.local>
2023-02-17 10:24:16 +0100

    12

---
+++ Module01/ex03/generator.py
@@ -0,0 +1,29 @@
+import random
+
+def generator(text, sep=" ", option=None):
+    '''Splits the text according to sep value and yield the substrings.
+    option precise if a action is performed to the substrings before it is yielded.
+    '''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    ret = text.split(sep)
+    if option == None:
+        for i in ret:
+            yield i
+    elif option == "shuffle":
+        random.shuffle(ret)
+        for i in ret:
+            yield i
+    elif option == "unique":
+        ret2 = list(dict.fromkeys(ret))
+        for i in ret2:
+            yield i
+    elif option == "sorted":
+        ret.sort(key=str.lower)
+        for i in ret:
+            yield i
+    else :
+        raise ValueError("this option is not defined")
+
+
+    
